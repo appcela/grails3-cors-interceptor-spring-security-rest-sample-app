230a43136948ccf31635fd354aa90c98d6e750f3
Ivan Chang <(none)>
2016-05-25 02:05:14 +0800

    removed default controller mapping; added OPTIONS mapping for CORS

--- grails-app/controllers/myrestapp/UrlMappings.groovy
+++ grails-app/controllers/myrestapp/UrlMappings.groovy
@@ -3,17 +3,14 @@ package myrestapp
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
 
         "/"(controller: 'application', action:'index')
-        "500"(view: '/error')
-        "404"(view: '/notFound')
+        "/500"(view: '/error')
+        "/404"(view: '/notFound')
 
         "/api/authors"(resources: 'author')
+        "/api/authors/$id?"(controller:'author', method: 'OPTIONS') // explicitly map OPTIONS method to "author" REST controller
         "/api/books"(resources: 'book')
+        "/api/books/$id?"(controller:'book', method: 'OPTIONS') // explicitly map OPTIONS method to "book" REST controller
     }
 }
