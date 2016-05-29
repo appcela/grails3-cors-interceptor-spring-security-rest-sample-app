6afaa90e283446c29d457a366096a0304a4a654d
Ivan Chang <ichang@appcela.com>
2016-05-29 12:17:07 +0800

    fixed duplicate CORS response headers issue by adding the missing spring security core filter config

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,6 +1,12 @@
 import grails3.cors.interceptor.SpringSecurityCorsFilter
+import org.springframework.boot.context.embedded.FilterRegistrationBean
 
 // Place your Spring DSL code here
 beans = {
     securityCorsFilter(SpringSecurityCorsFilter)
+
+    myFilterDeregistrationBean(FilterRegistrationBean) {
+        filter = ref('securityCorsFilter')
+        enabled = false
+    }
 }
